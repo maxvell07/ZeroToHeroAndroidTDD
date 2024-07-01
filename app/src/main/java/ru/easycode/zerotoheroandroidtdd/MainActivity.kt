7758c893213aa606d661a7519cf324f4b7fc4c64
Malik <bittiev.malik@mail.ru>
2024-07-02 01:20:58 +0300

    commit #1 task14 solved_1

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,38 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button1: Button
+    lateinit var textView: TextView
+    lateinit var progressbar:ProgressBar
+    lateinit var rootLayout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1 = findViewById(R.id.actionButton)
+        textView = findViewById(R.id.titleTextView)
+        progressbar = findViewById(R.id.progressBar)
+        rootLayout= findViewById(R.id.rootLayout)
+
+        button1.setOnClickListener {
+            button1.isEnabled = false
+            progressbar.visibility = View.VISIBLE
+            button1.postDelayed({
+                textView.visibility= View.VISIBLE
+                progressbar.visibility=View.GONE
+                button1.isEnabled=true
+                                },
+                3500)
+
+            textView.visibility= View.GONE
+        }
     }
+
 }
