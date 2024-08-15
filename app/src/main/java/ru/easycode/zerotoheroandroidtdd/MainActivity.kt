185a9b7501fdaf141f858c0cb380f347b2d057e3
Malik <bittiev.malik@mail.ru>
2024-08-16 00:02:44 +0300

    task20 solved
    Signed-off-by: Malik <bittiev.malik@mail.ru>

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.actionButton.setOnClickListener{
+            binding.titleTextView.text= binding.inputEditText.text.toString()
+            binding.inputEditText.setText("")
+        }
     }
 }
